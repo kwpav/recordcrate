a710077fbf365308de0e5274d055d1cdbba1612a
Kevin Pavao <kpavao84@gmail.com>
2018-06-17 06:06:22 -0500

    change single quote to double quote

--- ui/src/components/Main.jsx
+++ ui/src/components/Main.jsx
@@ -2,20 +2,18 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  Link,
   Switch,
-  Redirect
 } from 'react-router-dom';
 import AlbumList from './AlbumList';
 
 function Main() {
   return (
-   <main>
-     <Switch>
-      <Route exact path='/' component={AlbumList}/>
-     </Switch>
-   </main>
+    <main>
+      <Switch>
+        <Route exact path="/" component={AlbumList} />
+      </Switch>
+    </main>
   );
- }
+}
 
 export default Main;
