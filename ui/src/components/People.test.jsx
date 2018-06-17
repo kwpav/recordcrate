14df19064807eab13eb40222d6d48a0d65bf507b
Kevin Pavao <kpavao84@gmail.com>
2018-06-17 07:37:27 -0500

    add jest to eslint, use jsx for App test

--- ui/src/components/People.test.jsx
+++ ui/src/components/People.test.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import People from './People';
 
-
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM
