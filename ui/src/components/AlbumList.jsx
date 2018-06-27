13ca816b788ae0687805d51cc5b106acbe06a413
Kevin Pavao <kpavao84@gmail.com>
2018-06-26 22:53:38 -0500

    add keys to AlbumList

--- ui/src/components/AlbumList.jsx
+++ ui/src/components/AlbumList.jsx
@@ -5,16 +5,19 @@ import AlbumCard from './AlbumCard';
 const data = [
   [
     {
+      id: 1,
       band: 'The Band',
       album: 'The Last Waltz',
       date: '1/1/1970',
     },
     {
+      id: 2,
       band: 'The Band',
       album: 'Stage Freight',
       date: '1/1/1970',
     },
     {
+      id: 3,
       band: 'The Band',
       album: 'Music from Big Pink',
       date: '1/1/1970',
@@ -22,17 +25,20 @@ const data = [
   ],
   [
     {
+      id: 4,
       band: 'The Grateful Dead',
       album: 'American Beauty',
       date: '1/1/1980',
     },
     {
+      id: 5,
       band: 'The Grateful Dead',
       album: 'Workingmans Dead',
       date: '1/1/1980',
 
     },
     {
+      id: 6,
       band: 'The Grateful Dead',
       album: 'Barton Hall',
       date: '1/1/1980',
@@ -43,10 +49,10 @@ const AlbumList = props => (
   <div className="container">
     <h1 className="page-title">{props.pageTitle}</h1>
     <div className="card-group record-collection">
-      {data.map(group => (
-        <div className="record-shelf row">
+      {data.map((group, i) => (
+        <div key={i} className="record-shelf row">
           {group.map(release => (
-            <div className="col-md-4">
+            <div key={release.id} className="col-md-4">
               <AlbumCard albumName={release.album} bandName={release.band} />
             </div>
           ))}
