8ef90a832456c306842b07387287a2a7d280c516
Kevin Pavao <kpavao84@gmail.com>
2018-06-26 23:37:03 -0500

    initial SignIn test

---
+++ ui/src/components/SignIn.test.jsx
@@ -0,0 +1,13 @@
+import { MemoryRouter } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SignIn from './SignIn';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM
+    .render(<MemoryRouter>
+              <SignIn />
+            </MemoryRouter>, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
